9ed80ab1fc5d35536a70ed1402ee8c0560a4bd09
gabrielfernando23 <gabriel.ssilva@sptech.school>
2024-06-04 20:54:40 -0300

    p

--- src/main/java/br/com/sanittas/app/api/configuration/springdoc/security/SecurityConfig.java
+++ src/main/java/br/com/sanittas/app/api/configuration/springdoc/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://3.228.35.151")
+                .allowedOriginPatterns("http://54.92.146.82")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
